e6219d60e3a9e73b6d19e102a0a39768b5434d3f
Daniel Nogueira <daniel.nogueira.filho@gmail.com>
2023-10-11 00:49:32 -0300

    Adição de arquivo separado da Amostra Estratificada a ser juntado com o script principal

--- amostra.py
+++ amostra.py
@@ -1,22 +1,119 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('Dados.csv')
-# print(df.head(n=10))
-# print(df.info())
 
-print(df["Modulo"].value_counts())
 
-# Divide os dados em treinamento e teste estratificados
-x_train, x_test, y_train, y_test = train_test_split(df.drop('Modulo', 
-                                                            axis=1), 
-                                                            df['Modulo'], 
-                                                            test_size=0.90, 
-                                                            random_state=1, 
-                                                            stratify=df['Modulo'])
+dataModulo1 = df[df["Modulo"] == 1]
+dataModulo2 = df[df["Modulo"] == 2]
+dataModulo3 = df[df["Modulo"] == 3]
+dataModulo4 = df[df["Modulo"] == 4]
+dataModulo5 = df[df["Modulo"] == 5]
+dataSample = df.sample(round(len(df)*0.1))
 
-# Cria um novo DataFrame com base nos dados de treinamento
-amostra_estratificada = pd.concat([x_train, y_train], axis=1)
+print(dataModulo1)
 
-# Exibe as contagens de módulo na amostra estratificada
-print(amostra_estratificada['Modulo'].value_counts())
+# Proporção das Estratificações:
+fator_modulo1 = len(dataModulo1)/len(df)
+proporcao_modulo1 = fator_modulo1 * len(df)*0.1
+
+fator_modulo2 = len(dataModulo2)/len(df)
+proporcao_modulo2 = fator_modulo2 * len(df)*0.1
+
+fator_modulo3 = len(dataModulo3)/len(df)
+proporcao_modulo3 = fator_modulo3 * len(df)*0.1
+
+fator_modulo4 = len(dataModulo4)/len(df)
+proporcao_modulo4 = fator_modulo4 * len(df)*0.1
+
+fator_modulo5 = len(dataModulo5)/len(df)
+proporcao_modulo5 = fator_modulo5 * len(df)*0.1
+
+amostraM1 = dataModulo1.sample(round(proporcao_modulo1))
+print(amostraM1)
+print()
+amostraM2 = dataModulo2.sample(round(proporcao_modulo2))
+print("Módulo2", amostraM2)
+print()
+amostraM3 = dataModulo3.sample(round(proporcao_modulo3))
+print("Módulo3", amostraM3)
+print()
+amostraM4 = dataModulo4.sample(round(proporcao_modulo4))
+print("Módulo4", amostraM4)
+print()
+amostraM5 = dataModulo5.sample(round(proporcao_modulo5))
+print("Módulo5", amostraM5)
+
+
+# Dataframe das Amostras
+amostras = [amostraM1, amostraM2, amostraM3, amostraM4, amostraM5]
+amostra_unificada = pd.concat(amostras)
+
+print(amostra_unificada)
+
+
+# Medidas da Amostra
+media_amostra_altura = amostra_unificada['Altura'].mean()
+media_amostra_peso = amostra_unificada['Peso'].mean()
+media_amostra_idade = amostra_unificada['Idade'].mean()
+media_amostra_IMC = amostra_unificada['Imc'].mean()
+
+moda_amostra_altura = amostra_unificada['Altura'].mode()
+moda_amostra_peso = amostra_unificada['Peso'].mode()
+moda_amostra_idade = amostra_unificada['Idade'].mode()
+moda_amostra_IMC = amostra_unificada['Imc'].mode()
+
+mediana_amostra_altura = amostra_unificada['Altura'].median()
+mediana_amostra_peso = amostra_unificada['Peso'].median()
+mediana_amostra_idade = amostra_unificada['Idade'].median()
+mediana_amostra_IMC = amostra_unificada['Imc'].median()
+
+variacao_amostra_altura = amostra_unificada['Altura'].var()
+variacao_amostra_peso = amostra_unificada['Peso'].var()
+variacao_amostra_idade = amostra_unificada['Idade'].var()
+variacao_amostra_IMC = amostra_unificada['Imc'].var()
+
+desvio_padrao_amostra_altura = amostra_unificada['Altura'].std()
+desvio_padrao_amostra_peso = amostra_unificada['Peso'].std()
+desvio_padrao_amostra_idade = amostra_unificada['Idade'].std()
+desvio_padrao_amostra_IMC = amostra_unificada['Imc'].std()
+
+coeficiente_variacao_amostra_altura = (
+    desvio_padrao_amostra_altura / media_amostra_altura) * 100
+coeficiente_variacao_amostra_peso = (
+    desvio_padrao_amostra_peso / media_amostra_peso) * 100
+coeficiente_variacao_amostra_idade = (
+    desvio_padrao_amostra_idade / media_amostra_idade) * 100
+coeficiente_variacao_amostra_IMC = (
+    desvio_padrao_amostra_IMC / media_amostra_IMC) * 100
+
+print('------AMOSTRA------')
+print('Média da altura: ', media_amostra_altura)
+print('Média do peso: ', media_amostra_peso)
+print('Média da idade: ', media_amostra_idade)
+print('Média do IMC: ', media_amostra_IMC)
+
+print('Moda da altura: ', moda_amostra_altura)
+print('Moda do peso: ', moda_amostra_peso)
+print('Moda da idade: ', moda_amostra_idade)
+print('Moda do IMC: ', moda_amostra_IMC)
+
+print('Mediana da altura: ', mediana_amostra_altura)
+print('Mediana do peso: ', mediana_amostra_peso)
+print('Mediana da idade: ', mediana_amostra_idade)
+print('Mediana do IMC: ', mediana_amostra_IMC)
+
+print('Variância da altura: ', variacao_amostra_altura)
+print('Variância do peso: ', variacao_amostra_peso)
+print('Variância da idade: ', variacao_amostra_idade)
+print('Variância do IMC: ', variacao_amostra_IMC)
+
+print('Desvio padrão da altura: ', desvio_padrao_amostra_altura)
+print('Desvio padrão do peso: ', desvio_padrao_amostra_peso)
+print('Desvio padrão da idade: ', desvio_padrao_amostra_idade)
+print('Desvio padrão do IMC: ', desvio_padrao_amostra_IMC)
+
+print('Coeficiente de variação da altura: ',
+      coeficiente_variacao_amostra_altura)
+print('Coeficiente de variação do peso: ', coeficiente_variacao_amostra_peso)
+print('Coeficiente de variação da idade: ', coeficiente_variacao_amostra_idade)
+print('Coeficiente de variação do IMC: ', coeficiente_variacao_amostra_IMC)
--- script.py
+++ script.py
@@ -1,69 +1,104 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 df = pd.read_csv('Dados.csv')
 
+# dataframes de cada módulo e um com sample de 10%
+dataModulo1 = df[df["Modulo"] == 1]
+dataModulo2 = df[df["Modulo"] == 2]
+dataModulo3 = df[df["Modulo"] == 3]
+dataModulo4 = df[df["Modulo"] == 4]
+dataModulo5 = df[df["Modulo"] == 5]
+dataSample = df.sample(int(len(df)*0.1))
+
+# Medidas da População:
 media_altura = df['Altura'].mean()
 media_peso = df['Peso'].mean()
 media_idade = df['Idade'].mean()
-media_IMC = df['Imc'].mean()
+media_imc = df['Imc'].mean()
 
 mediana_altura = df['Altura'].median()
 mediana_peso = df['Peso'].median()
 mediana_idade = df['Idade'].median()
-mediana_IMC = df['Imc'].median()
+mediana_imc = df['Imc'].median()
 
 moda_altura = df['Altura'].mode()
 moda_peso = df['Peso'].mode()
 moda_idade = df['Idade'].mode()
-moda_IMC = df['Imc'].mode()
+moda_imc = df['Imc'].mode()
 
 variacao_altura = df['Altura'].var()
 variacao_peso = df['Peso'].var()
 variacao_idade = df['Idade'].var()
-variacao_IMC = df['Imc'].var()
+variacao_imc = df['Imc'].var()
 
 desvio_padrao_altura = df['Altura'].std()
 desvio_padrao_peso = df['Peso'].std()
 desvio_padrao_idade = df['Idade'].std()
-desvio_padrao_IMC = df['Imc'].std()
+desvio_padrao_imc = df['Imc'].std()
 
 coeficiente_variacao_altura = desvio_padrao_altura / media_altura * 100
 coeficiente_variacao_peso = desvio_padrao_peso / media_peso * 100
 coeficiente_variacao_idade = desvio_padrao_idade / media_idade * 100
-coeficiente_variacao_IMC = desvio_padrao_IMC / media_IMC * 100
-
-print('Média da altura: ', media_altura)
-print('Média do peso: ', media_peso)
-print('Média da idade: ', media_idade)
-print('Média do IMC: ', media_IMC)
-
-print('Mediana da altura: ', mediana_altura)
-print('Mediana do peso: ', mediana_peso)
-print('Mediana da idade: ', mediana_idade)
-print('Mediana do IMC: ', mediana_IMC)
-
-print('Moda da altura: ', moda_altura)
-print('Moda do peso: ', moda_peso)
-print('Moda da idade: ', moda_idade)
-print('Moda do IMC: ', moda_IMC)
-
-print('Variância da altura: ', variacao_altura)
-print('Variância do peso: ', variacao_peso)
-print('Variância da idade: ', variacao_idade)
-print('Variância do IMC: ', variacao_IMC)
-
-print('Desvio padrão da altura: ', desvio_padrao_altura)
-print('Desvio padrão do peso: ', desvio_padrao_peso)
-print('Desvio padrão da idade: ', desvio_padrao_idade)
-print('Desvio padrão do IMC: ', desvio_padrao_IMC)
-
-print('Coeficiente de variação da altura: ', coeficiente_variacao_altura)
-print('Coeficiente de variação do peso: ', coeficiente_variacao_peso)
-print('Coeficiente de variação da idade: ', coeficiente_variacao_idade)
-print('Coeficiente de variação do IMC: ', coeficiente_variacao_IMC)
+coeficiente_variacao_IMC = desvio_padrao_imc / media_imc * 100
 
 
-# Técnica de Amostragem Utilizada (Amostragem Aleatória Simples) (10% da População):
+print(f'Média da altura: {media_altura:.2f}')
+print(f'Média do peso: {media_peso:.2f}')
+print(f'Média da idade: {media_idade:.2f}')
+print(f'Média do IMC: {media_imc:.2f}')
+
+print(f'Mediana da altura:  {mediana_altura}')
+print(f'Mediana do peso: {mediana_peso}')
+print(f'Mediana da idade:  {mediana_idade}')
+print(f'Mediana do IMC:  {mediana_imc}')
+
+# print(f'Moda da altura: {moda_altura::.2f}')
+# print(f'Moda do peso: {moda_peso:.2f}')
+# print(f'Moda da idade: ', moda_idade)
+# print(f'Moda do IMC: ', moda_imc)
+
+# Moda vem como um Series no pandas(pois pode ter mais de uma moda). Tem que passar para list
+print(f'Moda da altura: {moda_altura.tolist()}')
+print(f'Moda da peso: {moda_peso.tolist()}')
+print(f'Moda da idade: {moda_idade.tolist()}')
+print(f'Moda da IMC: {moda_imc.tolist()}')
+
+print(f'Variância da altura: {variacao_altura:.4f}')
+print(f'Variância do peso: {variacao_peso:.4f}')
+print(f'Variância da idade: {variacao_idade:.4f}')
+print(f'Variância do IMC: {variacao_imc:.4f}')
+
+print(f'Desvio padrão da altura: {desvio_padrao_altura:.4f}')
+print(f'Desvio padrão do peso: {desvio_padrao_peso:.4f}')
+print(f'Desvio padrão da idade: {desvio_padrao_idade:.4f}')
+print(f'Desvio padrão do IMC: {desvio_padrao_imc:.4f}')
+
+print(f'Coeficiente de variação da altura: {coeficiente_variacao_altura:.4f}' )
+print(f'Coeficiente de variação do peso: {coeficiente_variacao_peso:.4f}' )
+print(f'Coeficiente de variação da idade: {coeficiente_variacao_idade:.4f}' )
+print(f'Coeficiente de variação do IMC: {coeficiente_variacao_imc:.4f}')
+
+plt.figure(1)
+boxplot1 = df.boxplot(column=['Altura'])
+plt.title("Box plot Altura")
+
+plt.figure(2)
+boxplot2 = df.boxplot(column=['Peso'])
+plt.title("Box plot Peso")
+
+plt.figure(3)
+boxplot3 = df.boxplot(column=['Idade'])
+plt.title("Box plot Idade")
+
+plt.figure(4)
+boxplot4 = df.boxplot(column=['Imc'])
+plt.title("Box plot IMC")
+
+plt.show()
+
+# Amostragem Estratificada:
 amostra = df.sample(frac=0.10)
 
 print(amostra)
@@ -72,7 +107,6 @@ media_amostra_altura = amostra['Altura'].mean()
 media_amostra_peso = amostra['Peso'].mean()
 media_amostra_idade = amostra['Idade'].mean()
 media_amostra_IMC = amostra['Imc'].mean()
-
 
 moda_amostra_altura = amostra['Altura'].mode()
 moda_amostra_peso = amostra['Peso'].mode()
@@ -103,6 +137,7 @@ coeficiente_variacao_amostra_idade = (
 coeficiente_variacao_amostra_IMC = (
     desvio_padrao_amostra_IMC / media_amostra_IMC) * 100
 
+print('------AMOSTRA------')
 print('Média da altura: ', media_amostra_altura)
 print('Média do peso: ', media_amostra_peso)
 print('Média da idade: ', media_amostra_idade)
